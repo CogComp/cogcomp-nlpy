32037d9d00be830dcf54797102663dda1715d152
Bhargav Mangipudi <bhargav.mangipudi@gmail.com>
2017-03-13 14:53:39 -0500

    Update version to 0.5.dev0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-VERSION = '0.4.dev0'
+VERSION = '0.5.dev0'
 
 CLASSIFIERS = [
     'Development Status :: 3 - Alpha', 'Intended Audience :: Science/Research',
