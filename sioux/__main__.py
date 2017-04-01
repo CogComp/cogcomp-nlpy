28be8b16da7b1f8833f273cd46f03b6914dd205a
Bhargav Mangipudi <bhargav.mangipudi@gmail.com>
2017-04-01 03:11:41 -0500

    Add some logic to download model jar files using Maven.

---
+++ sioux/__main__.py
@@ -0,0 +1,22 @@
+from __future__ import print_function
+
+import argparse
+
+from .download import main as download_main
+
+parser = argparse.ArgumentParser(prog='sioux')
+subparsers = parser.add_subparsers(help='commands')
+
+# Create the parser for download command
+parser_download = subparsers.add_parser('download', help='download help')
+parser_download.add_argument("--version", help="CogComp model version",
+                             required=False)
+parser_download.set_defaults(func=download_main)
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+
+    try:
+        args.func(args)
+    except AttributeError:
+        parser.print_help()
