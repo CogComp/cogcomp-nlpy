651f13af0bf8a9323787fc48a2e2e79ab92042d4
GHLgh <gluo2@illinois.edu>
2017-04-04 13:23:03 -0500

    Added Cython in requirements.txt and setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     license='Research and Academic Use License',
     keywords="NLP, natural language processing",
     packages=find_packages(exclude=['tests.*', 'tests']),
-    install_requires=['configparser', 'jnius_indra', 'protobuf', 'requests', 'six'],
+    install_requires=['configparser', 'Cython', 'jnius_indra', 'protobuf', 'requests', 'six'],
     package_data={'sioux': ['config/*.cfg']},
     classifiers=CLASSIFIERS,
     setup_requires=['pytest-runner'],
