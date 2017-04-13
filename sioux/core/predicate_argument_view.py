bbcc2016898c620684e348ddab7f0fdd468bcd34
GHLgh <gluo2@illinois.edu>
2017-04-13 11:47:41 -0500

    Added comments on new functions

--- sioux/core/predicate_argument_view.py
+++ sioux/core/predicate_argument_view.py
@@ -28,12 +28,19 @@ class PredicateArgumentView(View):
             if 'incoming_relations' not in constituent or len(constituent['incoming_relations']) == 0:
                 self.predicates.append(constituent)
 
-    # return a list of predicates (constituents)
     def get_predicates(self):
+        """
+        Function to return a list of predicates which are constituents with no incoming relations
+        @return: list of predicates
+        """
         return self.predicates
 
-    # list of outgoing relations
     def get_arguments(self, predicate):
+        """
+        Function to return a list of outgoing relations given a predicate
+        @param: predicate, the predicate to get outgoing relations from 
+        @return: a list of relations, None if the predicate is invalid
+        """
         if self._check_predicate(predicate):
             res = []
             for index in predicate['outgoing_relations']:
@@ -42,11 +49,21 @@ class PredicateArgumentView(View):
         return None
 
     def get_predicate_properties(self, predicate):
+        """
+        Function to get the properties of given predicate
+        @param: predicate, the predicate to get properties from
+        @return: a dictionary object contains following keys: predicate, SenseNumber
+        """
         if self._check_predicate(predicate):
             return predicate['properties']
         return None
 
     def _check_predicate(self, predicate):
+        """
+        Helper function to check if predicate is valid
+        @param: predicate, the predicate to evaluate
+        @return: True if the predicate is valid. False, otherwise
+        """
         if predicate not in self.predicates:
             logger.error("Predicate " + predicate + " not found")
             return False
