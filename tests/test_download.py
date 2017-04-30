4b2191daea3015a1678c0c304b2d186ac6d5d1be
GHLgh <gluo2@illinois.edu>
2017-04-29 19:56:35 -0500

    Added functionality to recover config
    If user delete the config file in model directory,
    then it will be recovered from package config.
    
    Fixed issue #43

---
+++ tests/test_download.py
@@ -0,0 +1,21 @@
+import unittest
+import sys
+import os
+import codecs
+
+from sioux import download
+
+test_config_folder = os.path.dirname(os.path.realpath(__file__))
+
+class TestDownload(unittest.TestCase):
+    def test_recover_model_config(self):
+        if os.path.exists(test_config_folder+'/config.cfg'):
+            os.remove(test_config_folder+'/config.cfg')
+        download.get_root_directory = lambda : test_config_folder
+        self.assertEqual(os.path.exists(test_config_folder+'/config.cfg'),False)
+        download.recover_model_config()
+        self.assertEqual(os.path.exists(test_config_folder+'/config.cfg'),True) 
+
+    def tearDown(self):
+        if os.path.exists(test_config_folder+'/config.cfg'):
+            os.remove(test_config_folder+'/config.cfg')
