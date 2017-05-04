8abd1834b23c157ac65a06cebaecbb56b9805b08
GHLgh <gluo2@illinois.edu>
2017-05-04 13:57:26 -0500

    Changed script according to comments

--- change_config_script.py
+++ change_config_script.py
@@ -1,11 +1,15 @@
 import configparser
 import codecs
 import os
+import sys
+
 from sioux import download
+
+PORT = sys.argv[1]
 
 # Get absolute path to config file in root directory
 config = configparser.ConfigParser()
-file_path = os.path.expanduser("~{0}.sioux{0}".format(os.path.sep)) + "config.cfg"
+file_path = os.path.join(os.path.expanduser(download.DEFAULT_CONFIG_ROOT_DIRECTORY),download.CONFIG_FILENAME)
 
 # Remove the config file and restore it such that it is consistent with the DEFAULT version in download.py
 os.remove(file_path)
@@ -16,7 +20,7 @@ with codecs.open(file_path, mode='r', encoding='utf-8') as f:
 
 # Change specific setting in config file
 # You should change PORT such that the tests are run in unlimited port
-config['remote_pipeline_setting']['api'] = "http://austen.cs.illinois.edu:PORT"
+config['remote_pipeline_setting']['api'] = "http://austen.cs.illinois.edu:" + PORT
 
 with codecs.open(file_path, mode='w', encoding='utf-8') as f:
     config.write(f)
