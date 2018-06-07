cc48f81ee496f868eef51368e262da397f3259f7
sanjay <sanjay@dickens.cs.illinois.edu>
2018-06-07 15:58:58 -0500

    Adding function to call server and create TextAnnotation given sentences and tokens rather than text

--- ccg_nlpy/local_pipeline.py
+++ ccg_nlpy/local_pipeline.py
@@ -44,6 +44,9 @@ class LocalPipeline(PipelineBase):
             from jnius import autoclass
             self.PipelineFactory = autoclass('edu.illinois.cs.cogcomp.pipeline.main.PipelineFactory')
             self.SerializationHelper = autoclass('edu.illinois.cs.cogcomp.core.utilities.SerializationHelper')
+            self.Tokenization = autoclass('edu.illinois.cs.cogcomp.nlp.tokenizer.Tokenizer$Tokenization')
+            self.IntPair = autoclass('edu.illinois.cs.cogcomp.core.datastructures.IntPair')
+            self.TextAnnotation = autoclass('edu.illinois.cs.cogcomp.core.datastructures.textannotation.TextAnnotation')
             # self.ProtobufSerializer = autoclass('edu.illinois.cs.cogcomp.core.utilities.protobuf.ProtobufSerializer')
             self.Boolean = autoclass('java.lang.Boolean')
             self.JString = autoclass('java.lang.String')
@@ -75,6 +78,7 @@ class LocalPipeline(PipelineBase):
                     logger.error(str(e))
 
         json = self.SerializationHelper.serializeToJson(text_annotation)
+        print(json)
 
         # path = os.path.expanduser('~') + "{0}.ccg_nlpy{0}".format(os.path.sep) + 'temp.temp'
         #
@@ -86,5 +90,40 @@ class LocalPipeline(PipelineBase):
         # message = TextAnnotation_pb2.TextAnnotationProto()
         # message.ParseFromString(proto_data)
         # proto_to_json = json_format.MessageToJson(message)
+
+        return json
+
+    def call_server_with_sentences(self, sentences, views):
+        """
+        Funtion to get preprocess text annotation from local pipeline
+
+        @param: sentences, the sentences to generate text annotation
+                on views, the views to generate
+        @return: raw text of the response from local pipeline
+        """
+        view_list = views.split(',')
+        tokens = [token for sent in sentences for token in sent]
+        char_offsets = []
+        count = 0
+        char_offsets_ints = []
+        for token in tokens:
+            char_offsets.append(self.IntPair(count, count+len(token)-1+1))
+            char_offsets_ints.append((count, count+len(token)-1+1))
+            count += len(token)+1
+        sentence_end_indices = []
+        count = 0
+        for sent in sentences:
+            sentence_end_indices.append(count+len(sent)-1+1)
+            count += len(sent)
+        text_annotation = self.TextAnnotation("", "", text, char_offsets, tokens, sentence_end_indices)
+        for view in view_list:
+            if (len(view.strip()) > 0):
+                try:
+                    self.pipeline.addView(text_annotation, self.JString(view.strip()))
+                except Exception as e:
+                    logger.error('Failed to add view ' + view.strip())
+                    logger.error(str(e))
+
+        json = self.SerializationHelper.serializeToJson(text_annotation)
 
         return json
