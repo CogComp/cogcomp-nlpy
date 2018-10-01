5c7790d7c0dfc2bb39ba7f12af7d528115f7884c
Shyam Upadhyay <shyamupa@gmail.com>
2018-10-01 15:05:34 -0400

    fixing based on comments from Nitish

--- server/model_wrapper_server.py
+++ server/model_wrapper_server.py
@@ -26,15 +26,6 @@ class DummyModel:
 
     def load_params(self):
         logging.info("loading model params ...")
-        logging.info("required views: %s", self.required_views())
-        logging.info("provides view: %s", self.get_view_name())
-
-    def required_views(self):
-        return ["TOKENS", "NER_CONLL"]
-
-    def get_view_name(self):
-        provided_view_name = "DUMMYVIEW"
-        return provided_view_name
 
     def inference_on_ta(self, docta, new_view_name):
         # This upcases each token. Test for TokenLabelView
@@ -55,13 +46,23 @@ class DummyModel:
 class ServerWrapper:
     def __init__(self, model):
         self.model = model
-        self.provided_view = model.get_view_name()
-        # right now, we let call the initialization inside the init of server
+        self.provided_view = "DUMMYVIEW"
+        self.required_views = ["TOKENS", "NER_CONLL"]
+        # right now, we let call the model load inside the init of server
         # this could have been done outside. Cannot say which is a better choice.
+        # THIS NEEDS TO BE DEFINED IN THE MODEL
         self.model.load_params()
         # We need a pipeline to create views that are required by our model (e.g. NER is needed for WIKIFIER etc.)
         self.pipeline = remote_pipeline.RemotePipeline()
+        logging.info("required views: %s", self.get_required_views())
+        logging.info("provides view: %s", self.get_view_name())
         logging.info("ready!")
+
+    def get_required_views(self):
+        return self.required_views
+
+    def get_view_name(self):
+        return self.provided_view
 
     def annotate(self):
         # we get something like "?text=<text>&views=<views>". Below two lines extract these.
@@ -77,7 +78,7 @@ class ServerWrapper:
             # The appelles server will fallback to the next remote server.
             return "VIEW NOT PROVIDED"
         # create a text ann with the required views for the model
-        required_views = ",".join(model.required_views())
+        required_views = ",".join(self.get_required_views())
         ta_json = self.pipeline.call_server(text=text, views=required_views)
         docta = TextAnnotation(json_str=ta_json)
         # send it to your model for inference
@@ -88,8 +89,12 @@ class ServerWrapper:
         return ta_json
 
 
-if __name__ == "__main__":
+def main():
     model = DummyModel()  # create your model object here
     wrapper = ServerWrapper(model=model)
     app.add_url_rule(rule='/annotate', endpoint='annotate', view_func=wrapper.annotate, methods=['GET'])
     app.run(host='localhost', port=5000)
+
+
+if __name__ == "__main__":
+    main()
