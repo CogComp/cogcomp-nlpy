9341ee0bae47ffa2f3cc2fc8d954d1f62ccd701d
Shyam Upadhyay <shyamupa@gmail.com>
2018-10-26 11:29:49 -0400

    refactored code -- server folder moved inside ccg_nlpy folder

--- ccg_nlpy/server/server/urls.py
+++ ccg_nlpy/server/server/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from server import views
+from ccg_nlpy.server import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
